4dbe52da5598c484378130abe7950b32aa8a4b9a
qinxiaoyun <qinxiaoyun001@lianjia.com>
2019-06-12 16:10:53 +0800

    add build sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+mvn clean package -Dmaven.test.skip=true
+cd dubbo-admin-distribution/target
+
+PID=$(ps -ef | grep dubbo-admin-0.2.0.jar | grep -v grep | awk '{ print $2 }')
+echo $PID
+if [ -z "$PID" ]
+then
+    echo application is already stopped...
+else
+    kill -9 $PID
+    echo killed $PID.
+fi
+
+nohup java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=9745,suspend=n -jar dubbo-admin-0.2.0.jar  &
+tail -f /data0/www/apps/dubbo-admin/dubbo-admin-distribution/target/nohup.out
